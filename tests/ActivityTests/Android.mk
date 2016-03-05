146f9b5d4a25727500b6a15e671d906e31032af2
Emirkan Acar <edorewiel@gmail.com>
2016-03-05 21:31:57 +0200

    Fix zenfone-old Build Error

--- tests/ActivityTests/Android.mk
+++ tests/ActivityTests/Android.mk
@@ -1,11 +1,11 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+#LOCAL_PATH:= $(call my-dir)
+#include $(CLEAR_VARS)
+#
+#LOCAL_SRC_FILES := $(call all-subdir-java-files)
+#
+#LOCAL_PACKAGE_NAME := ActivityTest
+#
+#LOCAL_MODULE_TAGS := tests
+#LOCAL_CERTIFICATE := platform
 
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_PACKAGE_NAME := ActivityTest
-
-LOCAL_MODULE_TAGS := tests
-LOCAL_CERTIFICATE := platform
-
-include $(BUILD_PACKAGE)
+#include $(BUILD_PACKAGE)
